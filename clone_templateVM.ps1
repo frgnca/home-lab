9833d7977502165ba9e8145b1f8cfb98bdbc6dc1
frgnca <fg@frgn.ca>
2018-03-10 16:07:01 -0500

    Add files via upload

---
+++ clone_templateVM.ps1
@@ -0,0 +1,223 @@
+﻿<#
+  Copyright (c) 2017-2018 Francois Gendron <fg@frgn.ca>
+  MIT License
+
+  clone_templateVM.ps1
+  PowerShell script that imports and starts a virtual machine
+  from an exported template
+
+
+  Requirements:
+  PuTTY http://www.putty.org/
+#>
+
+################################################################################
+# Set new virtual machine parameters
+$VirtualMachineName = "test1" # ToDo: test if already exists
+$VirtualHardDriveSize = 10GB #[10GB+] # ToDo: auto resize in ubuntu
+$VirtualMachineMemory = 2GB #[2GB+]
+$VirtualSwitchName = "vSwitch"
+$VirtualMachineLocation = "D:\VMs"
+$startAction = "StartIfRunning" #[Nothing, Start, StartIfRunning]
+$VirtualMachineIP = "192.168.1.31" # ToDo: Add dhcp posibility
+$VirtualMachineUser = "test"
+$templateVMName = "_template"
+########################
+# Set internal variables
+$VirtualMachineGeneration = 2
+$templateFolder = "D:\frgnca\Documents\_fg\VM"
+$bashFolder = "D:\frgnca\Documents\_fg\Scripts\bash"
+$VirtualMachineFolder = "$VirtualMachineLocation\$VirtualMachineName"
+$SnapshotFolder = $VirtualMachineFolder+"\Snapshots"
+$VHDFolder = $VirtualMachineFolder+"\Virtual Hard Disks"
+$templateConfig = (Get-Item "$templateFolder\$templateVMName\Virtual Machines\*.vmcx").FullName
+$fileContent = Get-Content "$bashFolder\setupTemplate.sh.old"
+$bashProfile = "$bashFolder\.bash_profile"
+$setupTemplate = "$bashFolder\setupTemplate.sh"
+$displayRAMsizeGB = $VirtualMachineMemory / 1024 /1024 / 1024
+$displayVHDsizeGB = $VirtualHardDriveSize / 1024 /1024 / 1024
+
+# Display VM parameters
+Write-Host "
+########################
+
+  clone_templateVM
+
+########################
+
+ VM to create: $VirtualMachineName
+          VHD:  $displayVHDsizeGB GB
+          RAM:   $displayRAMsizeGB GB
+           IP: $VirtualMachineIP
+  StartAction: $startAction
+         User: $VirtualMachineUser
+Only continue if the parameters are correct [CTRL+C to cancel]"
+
+# Chance to stop before proceeding
+Pause
+
+# Function to write unix style files <https://picuspickings.blogspot.ca/2014/04/out-unix-function-to-output-unix-text_17.html>
+function Out-Unix
+{
+    param ([string] $Path)
+
+    begin 
+    {
+        $streamWriter = New-Object System.IO.StreamWriter("$Path", $false)
+    }
+    
+    process
+    {
+        $streamWriter.Write(($_ | Out-String).Replace("`r`n","`n"))
+    }
+    end
+    {
+        $streamWriter.Flush()
+        $streamWriter.Close()
+    }
+}
+
+# ToDo+: Check parameters validity, duplicate host/vm name, IP address, folder, etc.
+
+# Display instructions
+Write-Host '########################
+
+Wait time 5 min ( 83% when done)
+'
+
+# Import virtual machine template
+Import-VM -Path $templateConfig -Copy -GenerateNewId -SmartPagingFilePath $VirtualMachineFolder -SnapshotFilePath $SnapshotFolder -VhdDestinationPath $VHDFolder -VirtualMachinePath $VirtualMachineFolder > $null
+
+# Rename virtual machine
+Rename-VM -Name $templateVMName -NewName $VirtualMachineName
+
+# Connect virtual machine to network
+Connect-VMNetworkAdapter -VMName $VirtualMachineName -SwitchName $VirtualSwitchName
+
+# Resize VHD
+Resize-VHD -Path $VHDFolder"\VHD.vhdx" -SizeBytes $VirtualHardDriveSize
+
+# Set virtual machine to boot from virtual hard drive
+$VHD = Get-VMHardDiskDrive -VMName $VirtualMachineName
+Set-VMFirmware -VMName $VirtualMachineName -FirstBootDevice $VHD
+
+# Change virtual machine memory
+Set-VMMemory $VirtualMachineName -StartupBytes $VirtualMachineMemory
+
+# Set virtual machine to disable SecureBoot
+Set-VMFirmware -VMName $VirtualMachineName -EnableSecureBoot Off
+
+# Set virtual machine start action
+Set-VM -Name $VirtualMachineName -AutomaticStartAction $startAction
+
+# Set virtual machine stop action (autosave)
+Set-VM -Name $VirtualMachineName -AutomaticStopAction Save
+
+# If integration service is not enabled
+$VMIntegr = Get-VMIntegrationService -VMName $VirtualMachineName | Where-Object -Property Name -EQ "Interface de services d’invité" | Select-Object Enabled
+if($VMIntegr.Enabled -ne "True")
+{
+    # Enable integration service
+    Enable-VMIntegrationService -Name "Interface de services d’invité" -VMName $VirtualMachineName #-Name "guest service interface"
+}
+
+# Start virtual machine
+Start-VM $VirtualMachineName
+
+# While virtual machine does not respond to ping
+while(-Not(Test-Connection "192.168.1.100" -Count 1 -Quiet))
+{
+    # Do nothing, try again
+}
+
+# Wait 5 seconds after virtual machine starts responding to ping
+# ToDo: find something else more acceptable
+sleep(5)
+
+# Copy setupTemplate.sh.old to setupTemplate.sh
+$fileContent | Out-Unix -Path $setupTemplate
+
+# Find and replace line containing "newHostname=" with $VirtualMachineName
+$i = -1
+$find = "newHostname="
+$replace = "newHostname=""$VirtualMachineName"""
+$fileContent | ForEach-Object {$i++; if($_ -match $find){ $fileContent[$i] = $replace; $fileContent | Out-Unix -Path $setupTemplate } }
+
+# Find and replace line containing "newUser=" with $VirtualMachineUser
+$i = -1
+$find = "newUser="
+$replace = "newUser=""$VirtualMachineUser"""
+$fileContent | ForEach-Object {$i++; if($_ -match $find){ $fileContent[$i] = $replace; $fileContent | Out-Unix -Path $setupTemplate } }
+
+# Find and replace line containing "newIP=" with $VirtualMachineIP
+$i = -1
+$find = "newIP="
+$replace = "newIP=""$VirtualMachineIP"""
+$fileContent | ForEach-Object {$i++; if($_ -match $find){ $fileContent[$i] = $replace; $fileContent | Out-Unix -Path $setupTemplate } }
+
+# Copy setupTemplate.sh from localhost to virtual machine
+$toVM = "/home/username/"
+Copy-VMFile $VirtualMachineName -SourcePath $setupTemplate -DestinationPath $toVM -CreateFullPath -FileSource Host -Force
+
+# Copy .bash_profile from localhost to virtual machine
+$scriptCall = "sudo ~/./setupTemplate.sh"
+$scriptCall | Out-Unix -Path $bashProfile
+$toVM = "/home/username/"
+Copy-VMFile $VirtualMachineName -SourcePath $bashProfile -DestinationPath $toVM -CreateFullPath -FileSource Host -Force
+
+# Open ssh session to template vm with PuTTY
+cd "C:\Program Files\PuTTY"
+.\putty.exe -ssh username@192.168.1.100 -pw "password"
+
+# Display instructions
+Write-Host '########################
+
+## Type "password"
+## Type a newly invented password
+## Retype your newly invented password
+
+Wait time  1+min ( 99% when done)
+ssh session will close
+'
+
+# While virtual machine is not off
+while((Get-VM $VirtualMachineName | Select-Object -Property State).State -ne "Off")
+{
+    # Wait for a second
+    sleep(1)
+}
+
+# Display instructions
+Write-Host "########################
+
+Wait time  1 min (100% when done)
+"
+
+# ToDo: close old putty window
+
+# Create snapshot "base"
+Get-VM -Name $VirtualMachineName | Checkpoint-VM -SnapshotName "base"
+
+# Start virtual machine
+Start-VM $VirtualMachineName
+
+# While virtual machine does not respond to ping
+while(-Not(Test-Connection $VirtualMachineIP -Count 1 -Quiet))
+{
+    # Do nothing, try again
+}
+
+# Wait 5 seconds after virtual machine starts responding to ping
+# ToDo: how to confirm ssh request won't be too soon?
+sleep(10)
+
+# Display instructions
+Write-Host "########################
+
+Done
+
+########################"
+
+# Open ssh session to newly configured virtual machine with PuTTY
+cd "C:\Program Files\PuTTY"
+.\putty.exe -ssh $VirtualMachineUser@$VirtualMachineIP
---
+++ create_templateVM.ps1
@@ -0,0 +1,295 @@
+﻿<#
+  Copyright (c) 2017-2018 Francois Gendron <fg@frgn.ca>
+  MIT License
+
+  create_templateVM.ps1
+  PowerShell script that creates, configures, and exports a template Hyper-V VM
+
+
+  Requirements:
+  PuTTY http://www.putty.org/
+#>
+
+################################################################################
+# Set template virtual machine parameters
+$VirtualMachineName = "_template"
+$VirtualHardDriveSize = 10GB
+$VirtualMachineMemory = 2GB
+$VirtualSwitchName = "vSwitch"
+$VirtualMachineLocation = "D:\VMs"
+$InstallationMediaLocation = "D:\media\ubuntu-16.04.2-server-amd64.iso"
+$exportPath = "D:\frgnca\Documents\_fg\VM"
+########################
+# Set internal variables
+$VirtualMachineGeneration = 2
+$VirtualHardDriveLocation = "$VirtualMachineLocation\$VirtualMachineName\Virtual Hard Disks\VHD.vhdx"
+$displayRAMsizeGB = $VirtualMachineMemory / 1024 /1024 / 1024
+$displayVHDsizeGB = $VirtualHardDriveSize / 1024 /1024 / 1024
+
+# Display VM parameters
+Write-Host "
+########################
+
+  create_templateVM
+
+########################
+
+  VM Name: $VirtualMachineName
+  VM VHD:   $displayVHDsizeGB GB
+  VM RAM:    $displayRAMsizeGB GB
+Only continue if the parameters are correct
+#  -> just press Enter
+## -> do something, then press Enter"
+
+# Chance to stop before proceeding
+Pause
+
+# Display instructions
+Write-Host "
+########################
+
+Wait time  1 min (   1% when done)
+localhost console will open"
+
+# Create virtual machine
+New-VM -Name $VirtualMachineName -MemoryStartupBytes $VirtualMachineMemory -Generation $VirtualMachineGeneration -NewVHDPath $VirtualHardDriveLocation -NewVHDSizeBytes $VirtualHardDriveSize -Path $VirtualMachineLocation -SwitchName $VirtualSwitchName > $null
+
+# Add DVD drive with installation media to virtual machine
+Add-VMDvdDrive -VMName $VirtualMachineName -ControllerNumber 0 -ControllerLocation 1 -Path $InstallationMediaLocation
+
+# Set virtual machine to boot from DVD drive
+$DVDDrive = Get-VMDvdDrive -VMName $VirtualMachineName
+Set-VMFirmware -VMName $VirtualMachineName -FirstBootDevice $DVDDrive
+
+# Set virtual machine to disable SecureBoot
+Set-VMFirmware -VMName $VirtualMachineName -EnableSecureBoot Off
+
+# Create a pre-install snapshot
+Checkpoint-VM -VMName $VirtualMachineName -SnapshotName "pre-install" > $null
+
+# Start virtual machine
+Start-VM -Name $VirtualMachineName > $null
+
+# Display ubuntu installation instructions
+Write-Host '
+########################
+
+  Installation
+  GNU GRUB
+#  Select <Install Ubuntu Server>
+  Select a language
+#  Select <English>
+  Select your location
+## Select <Canada>
+  Configure the keyboard
+  Detect keyboard layout:
+#  Select <No>
+  Country of origin for the keyboard:
+## Select <French (Canada)>
+  Keyboard layout:
+#  Select <French (Canada)>
+
+Wait time  1 min (   3% when done)
+
+  Hostname:
+#  Type "ubuntu"
+  Set up users and passwords
+  Full name for the new user:
+#  Press Enter
+  Username for your account:
+## Type "username"
+  Choose a password for the new user:
+## Type "password"
+  Re-enter password to verify:
+## Type "password"
+  Encrypt your home directory?
+#  Select <No>
+  Configure the clock
+  Is this time zone correct?
+## Select <No>
+  Select your time zone:
+## Select <Eastern>
+  Partition disks
+  Partitioning method:
+#  Select <Guided - use entire disk and set up LVM>
+  Select disk to partition:
+#  Select <SCSI1 (0,0,0) (sda) - 10.7 GB Msft Virtual Disk>
+  Write the changes to disks and configure LVM?
+## Select <Yes>
+  Amount of volume group to use for guided partitioning:
+#  Press Enter
+  Force UEFI installation?
+## Select <Yes>
+  Finish partitioning and write changes to disk
+  Write the changes to disks?
+## Select <Yes>
+
+Wait time  5 min (  11% when done)
+
+  Configure the package manager
+  HTTP proxy information (blank for none):
+#  Press Enter
+
+Wait time  1 min (  12% when done)
+
+  Configure tasksel
+  How do you want to manage upgrades on this system?
+## Select <Install security updates automatically>
+  Software selection
+  Choose software to install:
+## Select with spacebar <Samba file server> and <openSSH server>
+
+Wait time 25+min (  51% when done)
+
+  Finish the installation
+#  Select <Continue>
+
+Wait time  1 min (  52% when done)'
+
+# Connect to virtual machine video console
+vmconnect.exe localhost $VirtualMachineName
+
+# While tickcount of virtual machine keeps going up (reboots too quickly, cannot catch "Off" state)
+$previousTickcount = (Get-VM -Name $VirtualMachineName).Uptime.Ticks
+while((Get-VM -Name $VirtualMachineName).Uptime.Ticks -gt $previousTickcount)
+{
+    # Update tickcount
+    $previousTickcount = (Get-VM -Name $VirtualMachineName).Uptime.Ticks
+}
+
+# Make sure virtual machine stays stopped
+Stop-VM $VirtualMachineName -Force > $null
+
+# While virtual machine is not off
+while((Get-VM -Name $VirtualMachineName).State -ne "Off")
+{
+    # Wait a second
+    sleep(1)
+}
+
+# Create a post-install snapshot
+Checkpoint-VM -VMName $VirtualMachineName -SnapshotName "post-install" > $null
+
+# Start virtual machine
+Start-VM $VirtualMachineName > $null
+
+# Display instructions
+Write-Host '
+########################
+
+  Login with localhost console (username:password)
+## Type "username"
+## Type "password"
+  Find IP address (192.168.1.???)
+## Type "ifconfig"
+## Type the IP address bellow when found'
+
+# Get temporary IP address
+$tempIP = Read-Host -Prompt "Temporary IP address"
+
+# While virtual machine does not respond to ping
+while(-Not(Test-Connection $tempIP -Count 1 -Quiet))
+{
+    # Display instructions
+    Write-Host "$tempIP unreachable"
+    
+    # Get temporary IP address
+    $tempIP = Read-Host -Prompt "Temporary IP address"
+}
+
+# ToDo: Close localhost console
+# ToDo: wget a script that does the following instead of pasting commands
+
+# Display instructions 1 of 5
+$command = 'sudo ufw allow ssh > /dev/null 2>&1 && sudo ufw allow 137 > /dev/null 2>&1 && sudo ufw allow 138 > /dev/null 2>&1 && sudo ufw allow 139 > /dev/null 2>&1 && sudo ufw allow 445 > /dev/null 2>&1 && sudo ufw --force enable > /dev/null 2>&1 && sudo sed -i "s#iface eth0 inet dhcp#iface eth0 inet static#" /etc/network/interfaces > /dev/null 2>&1 && echo -e "  address 192.168.1.100\n  netmask 255.255.255.0\n  gateway 192.168.1.1\n  dns-nameservers 8.8.8.8 8.8.4.4" | sudo tee -a /etc/network/interfaces > /dev/null 2>&1 && echo -e "[root]\n   comment = read only\n   path = /\n   browsable = yes\n   read only = yes\n   guest ok = yes" | sudo tee -a /etc/samba/smb.conf > /dev/null 2>&1'
+$command | clip
+Write-Host '
+########################
+
+## Paste "'$command'"'
+Write-Host '## Type "password"' # ToDo: sudo for this session
+
+# Open ssh session to temporary IP of virtual machine with PuTTY with username:password
+cd "C:\Program Files\PuTTY"
+.\putty.exe -ssh username@$tempIP -pw "password"
+
+# Wait for user to be ready to continue
+Pause
+
+# Display instructions 2 of 5
+$command = "sudo apt-get update > /dev/null 2>&1 && sudo apt-get -y upgrade > /dev/null 2>&1"
+$command | clip
+Write-Host '
+########################
+
+## Paste "'$command'"'
+Write-Host "
+Wait time 15+min (  75% when done)"
+
+# Wait for user to be ready to continue
+Pause
+
+# Display instructions 3 of 5
+$command = "sudo apt-get -y install --install-recommends linux-virtual-lts-xenial > /dev/null 2>&1"
+$command | clip
+Write-Host '
+########################
+
+## Paste "'$command'"'
+Write-Host "
+Wait time  5 min (  83% when done)"
+
+# Wait for user to be ready to continue
+Pause
+
+# Display instructions 4 of 5
+$command = "sudo apt-get -y install --install-recommends linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial > /dev/null 2>&1"
+$command | clip
+Write-Host '
+########################
+
+## Paste "'$command'"'
+Write-Host "
+Wait time  1 min ( 85% when done)"
+
+# Wait for user to be ready to continue
+Pause
+
+# Display instructions 5 of 5
+$command = "sudo shutdown now"
+$command | clip
+Write-Host '
+########################
+
+## Paste "'$command'"'
+
+Write-Host "
+Wait time 10 min (100% when done)
+ssh session will close"
+
+# While virtual machine is not off
+while((Get-VM $VirtualMachineName).State -ne "Off")
+{
+    # Wait 1 second
+    sleep(1)
+}
+
+# Create an official template snapshot
+Checkpoint-VM -VMName $VirtualMachineName -SnapshotName "template" > $null
+
+# Export virtual machine
+Export-VM -Name $VirtualMachineName -Path $exportPath
+
+# Remove virtual machine template from Hyper-V
+Remove-VM $VirtualMachineName -Force
+
+# Delete virtual machine folder
+Remove-Item -Recurse -Force "$VirtualMachineLocation\$VirtualMachineName"
+
+# Display instructions
+Write-Host "
+########################
+
+Done
+
+########################"
